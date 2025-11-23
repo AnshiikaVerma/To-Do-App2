dfabdd378729621a759c03651283c6a47dae2126
Priyanshu-sde <priyanshu.sde@gmail.com>
2025-11-24 01:28:57 +0530

    Init

---
+++ script.js
@@ -0,0 +1,29 @@
+const taskInput = document.getElementById('task-input');
+const addBtn = document.getElementById('add-btn'); 
+const taskList = document.getElementById('task-list');
+const themeToggle = document.getElementById('theme-toggle');
+
+addBtn.addEventListener('click', addTask);
+
+function addTask() {
+    const taskText = taskInput.value;
+     
+    const li = document.createElement('li');
+    li.innerHTML = `
+        <span>${taskText}</span>
+        <button class="delete-btn">Delete</button>
+    `;
+    
+    taskList.appendChild(li);
+    taskInput.value = '';
+
+}
+
+const deleteBtns = document.querySelectorAll('.delete-btn');
+deleteBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        e.target.parentElement.remove();
+    });
+});
+
+
